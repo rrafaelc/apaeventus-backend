c8863972ff45581486bc8307459da97186527d02
Rafael Costa <rafaelcosta8020@gmail.com>
2025-05-27 19:33:00 -0300

    feat(database): restructure database schema to use UUIDs for primary keys and update role defaults

--- prisma/migrations/20250527222941_init/migration.sql
+++ prisma/migrations/20250527222941_init/migration.sql
@@ -1,17 +1,17 @@
 -- CreateEnum
-CREATE TYPE "Role" AS ENUM ('ADMIN', 'SELLER', 'CUSTOMER');
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'USER');
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT,
+    "password" TEXT NOT NULL,
     "rg" TEXT,
     "cpf" TEXT,
     "cellphone" TEXT,
     "refreshToken" TEXT,
-    "role" "Role" NOT NULL DEFAULT 'CUSTOMER',
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -19,22 +19,8 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
-CREATE TABLE "Address" (
-    "id" SERIAL NOT NULL,
-    "street" TEXT NOT NULL,
-    "number" INTEGER NOT NULL,
-    "neighborhood" TEXT NOT NULL,
-    "city" TEXT NOT NULL,
-    "state" TEXT NOT NULL,
-    "zipCode" TEXT NOT NULL,
-    "userId" INTEGER NOT NULL,
-
-    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "Ticket" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "eventDate" TIMESTAMP(3) NOT NULL,
@@ -50,10 +36,9 @@ CREATE TABLE "Ticket" (
 
 -- CreateTable
 CREATE TABLE "TicketSale" (
-    "id" SERIAL NOT NULL,
-    "sellerId" INTEGER NOT NULL,
-    "customerId" INTEGER NOT NULL,
-    "ticketId" INTEGER NOT NULL,
+    "id" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "ticketId" TEXT NOT NULL,
     "used" BOOLEAN NOT NULL DEFAULT false,
     "pdfUrl" TEXT,
     "qrCodeUrl" TEXT,
@@ -64,6 +49,20 @@ CREATE TABLE "TicketSale" (
     CONSTRAINT "TicketSale_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" INTEGER NOT NULL,
+    "neighborhood" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "zipCode" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
@@ -71,13 +70,10 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "User_cpf_key" ON "User"("cpf");
 
 -- AddForeignKey
-ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "TicketSale" ADD CONSTRAINT "TicketSale_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
 ALTER TABLE "TicketSale" ADD CONSTRAINT "TicketSale_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "TicketSale" ADD CONSTRAINT "TicketSale_ticketId_fkey" FOREIGN KEY ("ticketId") REFERENCES "Ticket"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
@@ -11,7 +12,7 @@ async function main() {
       name: 'Admin',
       email: 'admin@email.com',
       role: 'ADMIN',
-      password: null,
+      password: await bcrypt.hash('admin', 10),
       rg: null,
       cpf: null,
       refreshToken: null,
@@ -20,61 +21,61 @@ async function main() {
     },
   });
 
-  await prisma.ticket.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      title: '1ª TARDE ROCK',
-      description:
-        'Nossa primeira edição da Tarde Rock em prol a Apae de Itapira',
-      eventDate: new Date(
-        new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).setHours(19, 0, 0, 0), // Event in 30 days at 19:00
-      ),
-      imageUrl: null,
-      quantity: 100,
-      price: 30.0,
-      isActive: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
+  // await prisma.ticket.upsert({
+  //   where: { id: 1 },
+  //   update: {},
+  //   create: {
+  //     title: '1ª TARDE ROCK',
+  //     description:
+  //       'Nossa primeira edição da Tarde Rock em prol a Apae de Itapira',
+  //     eventDate: new Date(
+  //       new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).setHours(19, 0, 0, 0), // Event in 30 days at 19:00
+  //     ),
+  //     imageUrl: null,
+  //     quantity: 100,
+  //     price: 30.0,
+  //     isActive: true,
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  // });
 
-  await prisma.ticket.upsert({
-    where: { id: 2 },
-    update: {},
-    create: {
-      title: 'Festa Junina',
-      description: 'A melhor festa junina do Brasil',
-      imageUrl: null,
-      eventDate: new Date(
-        new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).setHours(19, 0, 0, 0), // Event in 30 days at 19:00
-      ),
-      quantity: 500,
-      price: 100.0,
-      isActive: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
+  // await prisma.ticket.upsert({
+  //   where: { id: 2 },
+  //   update: {},
+  //   create: {
+  //     title: 'Festa Junina',
+  //     description: 'A melhor festa junina do Brasil',
+  //     imageUrl: null,
+  //     eventDate: new Date(
+  //       new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).setHours(19, 0, 0, 0), // Event in 30 days at 19:00
+  //     ),
+  //     quantity: 500,
+  //     price: 100.0,
+  //     isActive: true,
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  // });
 
-  await prisma.ticket.upsert({
-    where: { id: 3 },
-    update: {},
-    create: {
-      title: '1ª QUEIMA DO ALHO',
-      description:
-        'Primeira edição da Queima do Alho juntamente com a tradicional Cavalgada e Passeio de Jeep',
-      eventDate: new Date(
-        new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).setHours(19, 0, 0, 0), // Event in 30 days at 19:00
-      ),
-      imageUrl: null,
-      quantity: 200,
-      price: 100.0,
-      isActive: true,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  });
+  // await prisma.ticket.upsert({
+  //   where: { id: 3 },
+  //   update: {},
+  //   create: {
+  //     title: '1ª QUEIMA DO ALHO',
+  //     description:
+  //       'Primeira edição da Queima do Alho juntamente com a tradicional Cavalgada e Passeio de Jeep',
+  //     eventDate: new Date(
+  //       new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).setHours(19, 0, 0, 0), // Event in 30 days at 19:00
+  //     ),
+  //     imageUrl: null,
+  //     quantity: 200,
+  //     price: 100.0,
+  //     isActive: true,
+  //     createdAt: new Date(),
+  //     updatedAt: new Date(),
+  //   },
+  // });
 }
 
 main()
