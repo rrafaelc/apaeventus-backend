e99dc69cdd0773858e55b850b763595906519928
Rafael Costa <rafaelcosta8020@gmail.com>
2025-05-31 11:27:10 -0300

    feat(auth): implement logout functionality with LogoutDto and update AuthService

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AccessTokenResponseDto } from './dtos/access-token-response.dto';
 import { LoginResponseDto } from './dtos/login-response.dto';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthenticatedRequest } from './requests/authenticated-request';
 import { LoginRequest } from './requests/login.request';
 import { RefreshTokenRequest } from './requests/refresh-token.request';
 
@@ -13,6 +24,15 @@ export class AuthController {
   @Post('login')
   async login(@Body() loginRequest: LoginRequest): Promise<LoginResponseDto> {
     return this.authService.login(loginRequest);
+  }
+
+  @UseGuards(AuthGuard)
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Post('logout')
+  async logout(@Request() { userId }: AuthenticatedRequest): Promise<void> {
+    if (!userId) throw new BadRequestException(['UserId not found in request']);
+
+    return this.authService.logout({ userId });
   }
 
   @HttpCode(HttpStatus.OK)
--- src/auth/interfaces/IAuthService.ts
+++ src/auth/interfaces/IAuthService.ts
@@ -1,10 +1,12 @@
 import { AccessTokenResponseDto } from '../dtos/access-token-response.dto';
 import { LoginResponseDto } from '../dtos/login-response.dto';
 import { LoginDto } from '../dtos/login.dto';
+import { LogoutDto } from '../dtos/logout.dto';
 import { RefreshTokenDto } from '../dtos/refresh-token.dto';
 
 export interface IAuthService {
   login(loginDto: LoginDto): Promise<LoginResponseDto>;
+  logout(logoutDto: LogoutDto): Promise<void>;
   refreshAccessToken(
     refreshTokenDto: RefreshTokenDto,
   ): Promise<AccessTokenResponseDto>;
